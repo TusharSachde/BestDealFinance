0ba9235b2c6defcd5369a573935035cd0bff77ec
Tushar <tushar@wohlig.com>
2015-03-26 18:32:03 +0530

    First Git with UI

---
+++ js/app.js
@@ -0,0 +1,123 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.overlaysWebView(true);
+            StatusBar.styleLightContent();
+        }
+    });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('login', {
+        url: "/login",
+        templateUrl: "templates/login.html",
+        controller: 'LoginCtrl'
+    })
+    
+    .state('register', {
+        url: "/register",
+        templateUrl: "templates/register.html",
+        controller: 'RegisterCtrl'
+    })
+    
+    .state('forgot', {
+        url: "/forgot",
+        templateUrl: "templates/forgot.html",
+        controller: 'ForgotCtrl'
+    })
+
+    .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+    })
+
+    .state('app.home', {
+        url: "/home",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/home.html",
+                controller: 'HomeCtrl'
+            }
+        }
+    })
+    
+    .state('app.myplan', {
+        url: "/myplan",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/myplan.html",
+                controller: 'MyplanCtrl'
+            }
+        }
+    })
+    
+    .state('app.finance', {
+        url: "/finance",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/finance.html",
+                controller: 'FinanceCtrl'
+            }
+        }
+    })
+    
+    .state('app.sme', {
+        url: "/sme",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/sme.html",
+                controller: 'SMECtrl'
+            }
+        }
+    })
+    
+    .state('app.genie', {
+        url: "/genie",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/genie.html",
+                controller: 'GenieCtrl'
+            }
+        }
+    })
+    
+    .state('app.listgenie', {
+        url: "/listgenie",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/listgenie.html",
+                controller: 'GenieCtrl'
+            }
+        }
+    })
+    
+    .state('app.thankyou', {
+        url: "/thankyou",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/thankyou.html",
+                controller: 'AppCtrl'
+            }
+        }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+});
---
+++ js/controllers.js
@@ -0,0 +1,28 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+})
+
+.controller('LoginCtrl', function($scope, $stateParams) {
+})
+
+.controller('RegisterCtrl', function($scope, $stateParams) {
+})
+
+.controller('ForgotCtrl', function($scope, $stateParams) {
+})
+
+.controller('HomeCtrl', function($scope, $stateParams) {
+})
+
+.controller('MyplanCtrl', function($scope, $stateParams) {
+})
+
+.controller('FinanceCtrl', function($scope, $stateParams) {
+})
+
+.controller('GenieCtrl', function($scope, $stateParams) {
+})
+
+.controller('SMECtrl', function($scope, $stateParams) {
+});
